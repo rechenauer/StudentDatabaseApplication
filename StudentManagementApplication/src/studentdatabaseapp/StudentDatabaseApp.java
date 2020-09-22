898e1eaa096823a413254c42d5f55a60b824e79a
sINFmarech <maximilian.rechenauer@stud.fh-rosenheim.de>
2020-09-22 18:02:10 +0200

    students are now being stored in an array

--- StudentManagementApplication/src/studentdatabaseapp/StudentDatabaseApp.java
+++ StudentManagementApplication/src/studentdatabaseapp/StudentDatabaseApp.java
@@ -1,12 +1,22 @@
 package studentdatabaseapp;
 
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
 public class StudentDatabaseApp {
     public static void main(String[] args) {
         //Ask how many new users we want to add
-        Student s = new Student();
-        s.enroll();
-        s.payTuition();
-        System.out.println(s.showInfo());
+        System.out.println("How many students do you want to add? ");
+        Scanner in = new Scanner(System.in);
+        int amountStudent = in.nextInt();
+        Student[] students = new Student[amountStudent];
+
         //Create n number of new students
+        for(int i = 0; i < amountStudent; i++){
+            students[i] = new Student();
+            students[i].enroll();
+            students[i].payTuition();
+            System.out.println(students[i].showInfo());
+        }
     }
 }
