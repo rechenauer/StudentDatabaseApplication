ac7214f51dd30d2d83a5eb744162cb2225663899
sINFmarech <maximilian.rechenauer@stud.fh-rosenheim.de>
2020-09-22 17:03:24 +0200

    enrolled courses are shown

--- StudentManagementApplication/src/studentdatabaseapp/Student.java
+++ StudentManagementApplication/src/studentdatabaseapp/Student.java
@@ -9,11 +9,12 @@ public class Student {
     private int year;
     private String studentID;
     private String courses;
-    private String tuitionBalance;
+    private int tuitionBalance = 0;
     private static int costOfCourse = 600;
     private static int id = 1000;
+
     //Constructor: prompt user to enter student's name and year
-    public Student(){
+    public Student() {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter student firstname: ");
         this.firstName = in.nextLine();
@@ -21,20 +22,42 @@ public class Student {
         this.lastName = in.nextLine();
         System.out.println("1 for Freshman \n2 for Sophmore \n3 for Junior\n4 for Senior \nEnter student class level: ");
         this.year = in.nextInt();
-        System.out.println(firstName + " "+ lastName + " "+ year);
+        System.out.println(firstName + " " + lastName + " " + year);
         setStudentID();
-        System.out.println(firstName + " "+ lastName+ " "+ year + " "+ studentID);
+        System.out.println(firstName + " " + lastName + " " + year + " " + studentID);
     }
+
     //Generate an ID
-    private void setStudentID(){
+    private void setStudentID() {
         //GradeLevel + ID
         id++;
         this.studentID = year + " " + id;
     }
 
     //Enroll in courses
+    public void enroll() {
+        boolean proceed = true;
+        //Get inside a loop until user hits Q
+        while (proceed) {
+            System.out.print("Enter course to enroll ('Q' to quit): ");
+            Scanner in = new Scanner(System.in);
+            String course = in.nextLine();
+            if (!course.equals("Q")) {
+                courses = courses + "\n" + course;
+                tuitionBalance += costOfCourse;
+            } else {
+                proceed = false;
+            }
 
-    //Pay tuition
+        }
+        System.out.println("Enrolled in: "+courses);
+        System.out.println("Tuition Balance: "+tuitionBalance);
 
-    //Show status
+    }
+
+
+
+//Pay tuition
+
+//Show status
 }
--- StudentManagementApplication/src/studentdatabaseapp/StudentDatabaseApp.java
+++ StudentManagementApplication/src/studentdatabaseapp/StudentDatabaseApp.java
@@ -3,7 +3,8 @@ package studentdatabaseapp;
 public class StudentDatabaseApp {
     public static void main(String[] args) {
         //Ask how many new users we want to add
-Student s = new Student();
+        Student s = new Student();
+        s.enroll();
         //Create n number of new students
     }
 }
