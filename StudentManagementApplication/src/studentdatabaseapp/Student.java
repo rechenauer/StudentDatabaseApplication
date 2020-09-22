974c9d8b0018d6c532082fd3a904834a977f712f
sINFmarech <maximilian.rechenauer@stud.fh-rosenheim.de>
2020-09-22 15:19:24 +0200

    studentID can be set

--- StudentManagementApplication/src/studentdatabaseapp/Student.java
+++ StudentManagementApplication/src/studentdatabaseapp/Student.java
@@ -2,14 +2,16 @@ package studentdatabaseapp;
 
 import java.util.Scanner;
 
+
 public class Student {
     private String firstName;
     private String lastName;
     private int year;
-    private int studentID;
+    private String studentID;
     private String courses;
     private String tuitionBalance;
     private static int costOfCourse = 600;
+    private static int id = 1000;
     //Constructor: prompt user to enter student's name and year
     public Student(){
         Scanner in = new Scanner(System.in);
@@ -20,9 +22,15 @@ public class Student {
         System.out.println("1 for Freshman \n2 for Sophmore \n3 for Junior\n4 for Senior \nEnter student class level: ");
         this.year = in.nextInt();
         System.out.println(firstName + " "+ lastName + " "+ year);
-
+        setStudentID();
+        System.out.println(firstName + " "+ lastName+ " "+ year + " "+ studentID);
     }
     //Generate an ID
+    private void setStudentID(){
+        //GradeLevel + ID
+        id++;
+        this.studentID = year + " " + id;
+    }
 
     //Enroll in courses
 
